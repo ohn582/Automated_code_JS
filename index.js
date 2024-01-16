0a8ceff80ce6ae2038d1b6b682856356e36e6b5b
Neal <neal.oh@pdware.com>
2024-01-16 09:11:54 -0500

    neal_Pdw.R1.base

--- index.js
+++ index.js
@@ -8,50 +8,22 @@ require('dotenv').config()
  */
 
 var options = {
-    theme: 'bootstrap',
-    // jsonFile: 'test/report/cucumber_report.json',
-    // output: 'test/report/cucumber_report.html',
-    jsonFile: 'cucumber_report.json',
-    output: 'report/cucumber_report.html',
-    reportSuiteAsScenarios: true,
-    scenarioTimestamp: true,
-    launchReport: true,
-    brandTitle: "PDWare ResourceFirst Report",
-    metadata: {
-        "App Version": process.env.APP_VERSION,
-        "Test Environment": process.env.TEST_ENVIRONMENT,
-        "Browser": `${process.env.BROWSER_NAME}  ${process.env.BROWSER_VERSION}`,
-        "Platform": platform(),
-        "Parallel": "Scenarios",
-        "Executed": "Local"
-    }
-    // failedSummaryReport: true,
-};
-    // reporter.generate(options);
-
-
-// const fs = require("fs");
-// const path = require("path");
-// console.log(path.toString() + "#################################");
-
-// const currentPath = path.join(__dirname, "report", "cucumber_report.html");
-// console.log(currentPath);
-// const destinationPath = path.join(__dirname, "history", `cucumber_report_${new Date().getTime()}.html`);
-// console.log(destinationPath);
-
-// fs.rename(currentPath, destinationPath, function (err) {
-//     if (err) {
-
-//         return true;
-//         //throw err
-//     } else {
-//         reporter.generate(options);
-//         console.log("Successfully moved the file!");
-//     }
-// });
-// reporter.generate(options);
-
-
+        theme: 'bootstrap',
+        jsonFile: 'cucumber_report.json',
+        output: 'report/cucumber_report.html',
+        reportSuiteAsScenarios: true,
+        scenarioTimestamp: true,
+        launchReport: true,
+        brandTitle: "PDWare ResourceFirst Report",
+        metadata: {
+            "App Version": process.env.APP_VERSION,
+            "Test Environment": process.env.TEST_ENVIRONMENT,
+            "Browser": `${process.env.BROWSER_NAME}  ${process.env.BROWSER_VERSION}`,
+            "Platform": platform(),
+            "Parallel": "Scenarios",
+            "Executed": "Local"
+        }
+    };
 
 /**
  * Moving reports to history
@@ -71,7 +43,3 @@ fs.rename(currentPath, destinationPath, function (err) {
         console.log("Successfully moved the file!");
     }
 });
-// reporter.generate(options);
-
-
-
